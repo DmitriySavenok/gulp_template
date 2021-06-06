f4f3ce8e1f3f9ca8ddb2c537ca136ef953f7411a
Савенок Дмитрий <36520766+DmitriySavenok@users.noreply.github.com>
2021-06-06 13:47:05 +0300

    Доб. обработку всех js файлов из src

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ let path = {
     src: {
         html: [source_folder + "/*.html", "!" + source_folder + "/_*.html"],
         css: source_folder + "/scss/style.scss",
-        js: source_folder + "/js/script.js",
+        js: source_folder + "/js/*",
         img: source_folder + "/img/**/*.+(png|jpg|gif|ico|svg|webp)",
         fonts: source_folder + "/fonts/*.ttf",
     },
